b2f7502fde20dd01133160744072a961cbab9587
Huiwen Pan <ga27keq@mytum.de>
2019-06-03 10:59:23 +0200

    add msg and srv

---
+++ src/Grasp.cpp
@@ -0,0 +1,24 @@
+/****** ROS HEADER *********/
+#include <ros/ros.h>
+#include <geometry_msgs/Point.h>
+
+
+/****** Msg HEADER *********/
+#include "tutorial5/Point.h"
+
+/****** Srv HEADER *********/
+#include "tutorial5/GraspRequest.h"
+
+
+
+
+
+
+
+int main(int argc, char**argv){
+
+    ros::init(argc, argv, "Grasp");
+    ros::NodeHandle nh;
+    
+
+}
