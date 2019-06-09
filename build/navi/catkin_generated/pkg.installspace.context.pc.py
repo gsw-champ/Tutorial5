d377f7f72ac2e62b83987768ae8636cadb7342be
lizhimao <lizhimao@hotmail.com>
2019-06-09 17:54:51 +0200

    delete devel and build

--- build/navi/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "actionlib;move_base_msgs;roscpp;geometry_msgs;std_srvs;moveit_ros_planning_interface".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "navi"
-PROJECT_SPACE_DIR = "/home/athomess19/Tutorial5/install"
-PROJECT_VERSION = "0.0.0"
